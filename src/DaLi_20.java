b291b59b8b16793771767391aafadf00765a6d2c
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-05-06 20:26:19 +0200

    20 init

---
+++ src/DaLi_20.java
@@ -0,0 +1,56 @@
+public abstract class DaLi_20 {
+
+    public static void main(String[] args) {
+        ex00();
+
+    }
+
+    public static void ex00() {
+    }
+    public static void ex01() {//Display words in descending alphabetical order
+    }
+    public static void ex02() {//Store numbers in a linked list
+    }
+    public static void ex03() {//Guessing the capitals
+    }
+    public static void ex04() {//Implement Comparable
+    }
+    public static void ex05() {//Combine colliding bouncing balls
+    }
+    public static void ex06() {//Use iterators on ArrayList
+    }
+    public static void ex07() {//Game: hangman
+    }
+    public static void ex08() {//Game: lottery
+    }
+    public static void ex09() {//Remove the largest ball first
+    }
+    public static void ex10() {//Perform set operations on priority queues
+    }
+    public static void ex11() {//Remove Consecutive Integers
+    }
+    public static void ex12() {//Create Stack from list
+    }
+    public static void ex13() {//Postfix notation
+    }
+    public static void ex14() {//Postfix notation
+    }
+    public static void ex15() {//Game: the 24-point card game
+    }
+    public static void ex16() {//Game: the 24-point card game
+    }
+    public static void ex17() {//Game: the 24-point card game
+    }
+    public static void ex18() {//Directory size
+    }
+    public static void ex19() {//Use Comparator
+    }
+    public static void ex20() {//Nonrecursive Tower of Hanoi
+    }
+    public static void ex21() {//Use Comparator
+    }
+    public static void ex22() {//Nonrecursive Tower of Hanoi
+    }
+    public static void ex23() {//Evaluate expression
+    }
+}
