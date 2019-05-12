765857474dae88f7d748d728b2ffe6083a128f21
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-05-12 12:08:20 +0200

    21 init

---
+++ src/DaLi_21.java
@@ -0,0 +1,55 @@
+public abstract class DaLi_21 {
+
+    public static void main(String[] args) {
+        ex00();
+
+    }
+
+    public static void ex00() {
+    }
+
+    public static void ex01() {//Perform set operations on hash sets
+    }
+
+    public static void ex02() {//Display nonduplicate names in ascending order
+    }
+
+    public static void ex03() {//Checking whether a key exists in a set
+    }
+
+    public static void ex04() {//Count consonants and vowels
+    }
+
+    public static void ex05() {//Syntax highlighting
+    }
+
+    public static void ex06() {//Count the occurrences of words/names
+    }
+
+    public static void ex07() {//Revise Listing 21.9, CountOccurrenceOfWords.java
+    }
+
+    public static void ex08() {//Count the occurrences of words in a text file
+    }
+
+    public static void ex09() {//Guess the capitals using maps
+    }
+
+    public static void ex10() {//Count the occurrences of each keyword
+    }
+
+    public static void ex11() {//Baby name popularity ranking
+    }
+
+    public static void ex12() {//Name for both genders
+    }
+
+    public static void ex13() {//Baby name popularity ranking
+    }
+
+    public static void ex14() {//Web crawler
+    }
+
+    public static void ex15() {//Addition quiz
+    }
+}
