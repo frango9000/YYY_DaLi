2a46c201630401697d9ebabca27ecd80a0584a31
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2020-02-25 10:35:26 +0100

    recursive list sort

--- src/auxp/ch18/RecursionTests.java
+++ src/auxp/ch18/RecursionTests.java
@@ -1,5 +1,7 @@
 package auxp.ch18;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 public class RecursionTests {
@@ -8,6 +10,12 @@ public class RecursionTests {
 //        IntStream.range(0, 20).forEach(i -> System.out.println(factorial(i)));
         System.out.println(isPalindrome("sddadds"));
         System.out.println(isPalindrome("sddadsds"));
+
+        int[] randomInts = IntStream.generate(() -> (int)(Math.random()*10)).limit(10).toArray();
+
+        System.out.println(Arrays.toString(randomInts));
+        sort(randomInts);
+        System.out.println(Arrays.toString(randomInts));
     }
 
 
@@ -45,5 +53,27 @@ public class RecursionTests {
         else if (string.charAt(start) != string.charAt(end))
             return false;
         else return isPalindrome(string,start+1, end-1);
+    }
+
+    public static void sort(int[] list){
+        sort(list, 0);
+    }
+
+    private static void sort(int[] list, int start) {
+        if(start>=list.length-1){
+            return;
+        }else {
+            int minIndex = start;
+            for (int i = minIndex+1; i < list.length; i++) {
+                if (list[i] < list[minIndex])
+                    minIndex = i;
+            }
+            if(minIndex!= start){
+                int temp = list[start];
+                list[start]= list[minIndex];
+                list[minIndex] = temp;
+            }
+            sort(list, start+1);
+        }
     }
 }
