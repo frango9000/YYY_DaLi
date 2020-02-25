8feae3200c9c9c3b2ea6738edbe24a4170112409
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2020-02-25 10:05:55 +0100

    recursive palindrome 1

--- src/auxp/ch18/RecursionTests.java
+++ src/auxp/ch18/RecursionTests.java
@@ -5,8 +5,9 @@ import java.util.stream.IntStream;
 public class RecursionTests {
 
     public static void main(String[] args) {
-        IntStream.range(0, 20).forEach(i -> System.out.println(factorial(i)));
-
+//        IntStream.range(0, 20).forEach(i -> System.out.println(factorial(i)));
+        System.out.println(isPalindrome("sddadds"));
+        System.out.println(isPalindrome("sddadsds"));
     }
 
 
@@ -25,6 +26,13 @@ public class RecursionTests {
             return 1;
         else
             return num * factorial(num - 1);
+    }
+
+    static boolean isPalindrome(String string){
+        boolean firstAndLastMatch = string.substring(0, 1).equalsIgnoreCase(string.substring(string.length() - 1));
+        if(string.length() < 3)
+            return firstAndLastMatch;
+        else return firstAndLastMatch && isPalindrome(string.substring(1, string.length() - 1));
     }
 
 }
